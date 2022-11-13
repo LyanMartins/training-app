2d591e0a52cfa08e4718c0c61220e47e99d46966
Lyan Martins <lyan.martins3@gmail.com>
2022-11-13 18:51:42 -0300

    initial login validation

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -1,10 +1,12 @@
 <template>
-  <form action="">
+  <form v-on:submit.prevent="onSubmit" class="mt-5">
     <label for="">Login</label>
-    <input class="input input-row mt-1" type="e-mail" name="" id="" placeholder="Email">
-    <input class="input input-row mt-1" type="password" placeholder="Senha">
+    <input 
+      class="input input-row mt-1" 
+      :class="{hasError: hasEmailInvalid}" type="e-mail" v-model="login.email" id="" required placeholder="Email">
+    <input class="input input-row mt-1" type="password" name="password" required placeholder="Senha">
     <div class="text-center mt-1"><a href="" class="">Esqueci minha senha</a></div>
-    <button v-on:click="" class="btn btn-row mt-1"> Entrar</button>
+    <button  class="btn btn-row mt-1"> Entrar</button>
   </form>
 </template>
 
--- src/views/Login/LoginComponent.ts
+++ src/views/Login/LoginComponent.ts
@@ -2,4 +2,40 @@ import { defineComponent } from "vue";
 
 export default defineComponent({
   components: {},
+
+  data() {
+    return {
+      login: {
+        email:'',
+        password:''
+      },
+      hasEmailInvalid:false
+    }
+  },
+
+  methods:{
+    onSubmit() {
+
+        console.log(this.hasEmailInvalid)
+    },
+    validateEmail(value: any){
+      if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(value))
+      {
+          this.hasEmailInvalid = true;
+      }
+      this.hasEmailInvalid = true;
+    },  
+  },
+
+  watch: {
+    'email.login'(value){
+      // binding this to the data value in the email input
+      this.login.email = value;
+      this.validateEmail(value);
+    },
+  },
+
+  mounted() {
+  },
+ 
 });
