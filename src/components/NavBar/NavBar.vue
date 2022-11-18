0a6ced3db9e60fddf331884e4ea6d8aaef49a72d
Lyan Martins <lyan.martins3@gmail.com>
2022-11-17 23:43:37 -0300

    initialized a navbar prototype

--- src/components/NavBar/NavBar.vue
+++ src/components/NavBar/NavBar.vue
@@ -1,7 +1,8 @@
 <template>
-  <nav class="top-navbar">
-    <i class="fa-thin fa-bars"></i>
-    navbar
+  <nav class="top-navbar wrapper third-column-center">
+    <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M7 34.55V33h34v1.55Zm0-9.8v-1.5h34v1.5ZM7 15v-1.55h34V15Z"/></svg>
+    .
+    <svg xmlns="http://www.w3.org/2000/svg" height="48" width="48"><path d="M11.15 42Q10 42 9.2 41.2t-.8-1.95v-28.5q0-1.15.8-1.95t1.95-.8h13.1v1.55h-13.1q-.45 0-.85.375-.4.375-.4.825v28.5q0 .45.4.825.4.375.85.375h13.1V42Zm22-10.5L32 30.4l4.65-4.65h-17.8v-1.5H36.6l-4.65-4.65 1.15-1.1 6.5 6.55Z"/></svg>
   </nav>
 </template>
 
